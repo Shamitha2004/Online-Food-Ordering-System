d76a599b025a9b1b0e883c312847772796cac432
Shamitha <shamithaaryapu@gmail.com>
2025-08-18 00:30:03 +0530

    Initial commit — Spring Boot skeleton

---
+++ onlineFoodOrdering.sql
@@ -0,0 +1,45 @@
+DROP DATABASE SCEM_Hibernate_DB;
+
+CREATE DATABASE SCEM_Hibernate_DB;
+
+USE SCEM_Hibernate_DB;
+
+show tables;
+
+
+SELECT * FROM Student_Account;
+
+
+
+
+SELECT * FROM Student1_Record;
+SELECT * FROM STUDENT1_RECORD_ACCOUNT;
+
+SELECT * FROM Student2_Record;
+
+SELECT * FROM Services;
+
+select * from Account;
+
+    
+select * from Student2_Record ;
+
+    
+select * from Student2_Record_Services ;
+    
+select * from Student_Record;
+
+select * from Employee;
+
+select * from cart_items;
+select * from cart_menu_items;
+select * from carts;
+select * from feedback;
+select * from menu_items;
+select * from order_menu_items ;
+select * from orders;
+select * from payments;
+select * from spring_session;
+select * from spring_session_attributes;
+select * from users;
+
